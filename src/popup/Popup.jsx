326d99733346efac3aa6c942e796821c04da611c
Rithik <rithikmotupalli@gmail.com>
2025-05-23 15:24:12 +0530

    added bg gradient

--- src/popup/Popup.jsx
+++ src/popup/Popup.jsx
@@ -16,7 +16,7 @@ const Popup = () => {
   };
 
   return (
-    <div className="flex flex-col gap-4 p-4 justify-center bg-white min-h-screen">
+    <div className="flex flex-col gap-4 p-4 justify-center bg-gradient-to-b from-white via-cyan-200 to-blue-100 min-h-screen">
       <h1 className='text-xl text-gray-800 font-bold text-center'><span className='text-blue-600'>App</span>ly <span className='text-blue-600'>Pi</span>lot</h1>
       <h2 className='text-gray-800 text-center'>ApplyPilot autofills common job application forms for you.</h2>
       
@@ -35,7 +35,7 @@ const Popup = () => {
       <p className='text-gray-800 text-center text-xs'>🛈 Click when you're on a job application page.</p>
 
       {/* Status area */}
-      <div className="text-center text-sm text-green-700 min-h-[1.5rem]">
+      <div className="text-center text-sm text-gray-700 min-h-[1.5rem]">
         {status}
       </div>
 
